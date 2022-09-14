f71dbae53b095495ebd1253f8860ab6e43e7d130
Peacebern <peace8282bern@gmail.com>
2022-09-14 17:53:09 +0100

    Update base_model.py
    updating basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -21,19 +21,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
-            if kwargs.get("created_at"):
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                    '%Y-%m-%dT%H:%M:%S.%f')
-            if kwargs.get("updated_at"):
-                self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                    '%Y-%m-%dT%H:%M:%S.%f')
-            for k,v in kwargs.items():
-                if k != "__class__":
-                    setattr(self, k, v)
-            if not self.id:
-                self.id = str(uuid.uuid4())
-
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+            
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
