0ffb5e339f912feb7dbdbce0e31c73a79cbb07da
Adebayo Samuel <eladebayoor@gmail.com>
2022-09-15 00:07:48 +0100

    [fix]: State to return list of cities on calling cities method

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                            cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns list of City instances related to state"""
+            from models import storage
+            list_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
