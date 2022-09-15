7ff6303d203f64a8ed195180dfaedd98dc2a13ad
Adebayo Samuel <eladebayoor@gmail.com>
2022-09-15 22:12:31 +0100

    [fix]: place_ammenities relationship added

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """class for amenities"""
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        name = ""
+
